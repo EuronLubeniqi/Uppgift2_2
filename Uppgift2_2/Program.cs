5c39723422f38bad8890475a9f1418a99b707bb1
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-10-03 14:04:55 +0200

    Add project files.

---
+++ Uppgift2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Uppgift2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hur långt hoppade Elin, mätt i meter");
+            string Elin = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma, mätt i meter");
+            string Alma = Console.ReadLine();
+            int A = int.Parse(Elin);
+            int B = int.Parse(Alma);
+            int skillnad = A - B;
+
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma");
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
